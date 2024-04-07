a06fa610248dbe771dbca8588b4373d012771d38
DominikEnder <dai2125@students.fhv.at>
2024-04-07 18:28:57 +0200

    umstellung auf Typescript

--- complete/src/main/react-map/src/App.tsx
+++ complete/src/main/react-map/src/App.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
-import MapGrid from './MapGrid/MapGrid';
 import KeyInputs from "./KeyInputs/KeyInputs";
+import MapGrid from './MapGrid/MapGrid';
 import style from './AppStyle.module.css';
-
-
 
 const App: React.FC = () => {
   const [xPos, setXPos] = useState<number>(2);
   const [yPos, setYPos] = useState<number>(2);
 
+
   const handleMove = (newX: number, newY: number) => {
     // Perform validation here if needed
     setXPos(newX);
     setYPos(newY);
+    console.log('App.tsx: handleMove: newX: ', newX, ' newY: ', newY);
   };
+
 
   return (
       <div className={style.root}>
-        <MapGrid xPos={xPos} yPos={yPos} onMove={handleMove} />
-        <KeyInputs/>
+          {/*<MapGrid canvasId="canvasId"/>*/}
+          {/*<MapGrid/>*/}
+          <KeyInputs/>
+          <MapGrid xPos={xPos} yPos={yPos} onMove={handleMove} />
       </div>
   );
 }
--- complete/src/main/react-map/src/MapGrid/MapGrid.tsx
+++ complete/src/main/react-map/src/MapGrid/MapGrid.tsx
@@ -1,99 +1,356 @@
-import React, { useEffect } from "react";
-import style from './MapGridStyle.module.css';
+import { Player } from "./Player";
+import {useEffect, useRef, useState} from "react";
+import {getAction, getColor, getUserId, getX, getY} from "../store";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
-interface MapGridProps {
-    xPos: number;
-    yPos: number;
-    onMove: (x: number, y: number) => void; // Define onMove prop
-}
+let otherPlayers: Player[] = [];
 
-const MapGrid: React.FC<MapGridProps> = ({ xPos, yPos, onMove }) => {
+const GameComponent = ({xPos, yPos, onMove}) => {
+
+    const canvasRef = useRef(null);
+
+    const player = useRef(new Player('null', 'test', 'blue', 7, 7));
+    const [playerOne, setPlayerOne] = useState(false);
+
+    const map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+
     useEffect(() => {
-        const handleMove = (event: KeyboardEvent) => {
-            let newXPos = xPos;
-            let newYPos = yPos;
 
-            switch (event.key) {
+        let id = getUserId();
+
+        const socket = new SockJS("http://localhost:8080/gs-guide-websocket");
+        const client = Stomp.over(socket);
+        client.connect({}, () => {
+            client.subscribe('/topic/register/', (message) => {
+                const response = JSON.parse(message.body);
+                console.log('Subscribe UserID ', response.userId + ' X ', response.x + ' Y ', response.y + ' Color ', response.color);
+                // player.setUserId(response.userId);
+                // player.
+                if(playerOne === false) {
+                    player.current.setAction(response.action);
+                    player.current.setUserId(response.userId);
+                    player.current.setColor(response.color);
+                    player.current.setX(response.x);
+                    player.current.setY(response.y);
+                    setPlayerOne(true);
+                    // gameLoop();
+                } else /*if(player.current.getUserId() !== response.userId)*/ {
+                    const newPlayer = new Player(response.action, response.userId, response.color, response.x, response.y);
+                    otherPlayers.push(newPlayer);
+                    console.log('OOOOOOOther Players instanziert', otherPlayers.length);
+                    // gameLoop();
+                }
+
+                // new Player(response.action, response.userId, 'yellow', response.x, response.y);
+
+            });
+            client.subscribe(`/topic/movement/`, (message) => {
+                const response = JSON.parse(message.body);
+                const id = response.userId;
+                const color = response.color;
+                const x = response.x;
+                const y = response.y;
+                const action = response.action;
+                callback(id, color, x, y, action);
+            });
+        });
+
+
+        const handleMove = (event: string) => {
+            let x = player.current.getX();
+            let y = player.current.getY();
+
+            switch (event) {
                 case 'ArrowUp':
-                    newXPos = Math.max(0, xPos - 1);
+                    console.log('ArrowUp');
+                    sendMovementUp(player.current.getY());
+                    // sendMovement(x , y);
                     break;
                 case 'ArrowDown':
-                    newXPos = Math.min(grid.length - 1, xPos + 1);
+                    console.log('ArrowDown');
+                    sendMovementDown(player.current.getY());
                     break;
                 case 'ArrowLeft':
-                    newYPos = Math.max(0, yPos - 1);
+                    console.log('ArrowLeft');
+                    sendMovementLeft(player.current.getX());
                     break;
                 case 'ArrowRight':
-                    newYPos = Math.min(grid[0].length - 1, yPos + 1);
+                    console.log('ArrowRight');
+                    sendMovementRight(player.current.getX());
                     break;
                 default:
                     break;
             }
-
-            // Check if the new position is valid (not on a # cell)
-            if (grid[newXPos][newYPos] !== '#') {
-                onMove(newXPos, newYPos);
-            }
         };
 
-        window.addEventListener('keydown', handleMove);
+        function sendMovementUp(y: number) {
+            let id = getUserId();
+            // console.log('ID ', id);
+            player.current.setAction('ArrowUp');
 
-        return () => {
-            window.removeEventListener('keydown', handleMove);
-        };
-    }, [xPos, yPos, onMove]);
-
-    const grid: any[][] = [
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '#'],
-        ['.', '.', '.', '#', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '.'],
-        ['#', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-        ['#', '#', '#', '.', '#', '#', '#', '#'],
-    ];
-
-    // Sight radius
-    const radius = 3;
-
-    const startRow = Math.max(0, xPos - radius);
-    const endRow = Math.min(grid.length - 1, xPos + radius);
-    const startCol = Math.max(0, yPos - radius);
-    const endCol = Math.min(grid[0].length - 1, yPos + radius);
-
-    // Load subGrid
-    const subGrid: any[][] = [];
-    for (let i = startRow; i <= endRow; i++) {
-        const row = [];
-        for (let j = startCol; j <= endCol; j++) {
-            row.push(grid[i][j]);
+            client.send(`/app/movement/${id}`, {}, JSON.stringify({
+                'action': player.current.getAction(),
+                'userId': player.current.getUserId(),
+                'color': player.current.getColor(),
+                'x': player.current.getX(),
+                'y': player.current.getY() + 1
+            }));
         }
-        subGrid.push(row);
-    }
 
-    // Center View
-    const paddingTop = Math.max(0, radius - xPos) * 30;
-    const paddingLeft = Math.max(0, radius - yPos) * 30;
+        function sendMovementDown(y: number) {
+            let id = getUserId();
+            player.current.setAction('ArrowDown');
+
+            client.send(`/app/movement/${id}`, {}, JSON.stringify({
+                'action': player.current.getAction(),
+                'userId': player.current.getUserId(),
+                'color': player.current.getColor(),
+                'x': player.current.getX(),
+                'y': player.current.getY() - 1
+            }));
+        }
+
+        function sendMovementLeft(x: number) {
+            let id = getUserId();
+            player.current.setAction('ArrowLeft');
+
+            client.send(`/app/movement/${id}`, {}, JSON.stringify({
+                'action': player.current.getAction(),
+                'userId': player.current.getUserId(),
+                'color': player.current.getColor(),
+                'x': player.current.getX() - 1,
+                'y': player.current.getY()
+            }));
+        }
+
+        function sendMovementRight(x: number) {
+            let id = getUserId();
+
+            player.current.setAction('ArrowRight');
+
+            client.send(`/app/movement/${id}`, {}, JSON.stringify({
+                'action': player.current.getAction(),
+                'userId': player.current.getUserId(),
+                'color': player.current.getColor(),
+                'x': player.current.getX() + 1,
+                'y': player.current.getY()
+            }));
+        }
+
+        function sendMovement(move: string) {
+            let id = getUserId();
+
+            client.send(`/app/movement/${id}`, {}, JSON.stringify({
+                'action': move,
+                'userId': player.current.getUserId(),
+                'color': player.current.getColor(),
+                'x': player.current.getX(),
+                'y': player.current.getY()
+            }));
+        }
+
+        function callback(id: string, color: string, x: number, y: number, action: string) {
+            if (id === player.current.getUserId()) {
+            console.log('Callback Function ' + id + ' X ', x + ' Y ', y + ' Color ', color + ' Action ', action);
+                if (action === 'ArrowUp') {
+                    player.current.setX(x - 1);
+                    player.current.draw(context);
+                    // xPos = xPos - 1;
+                } else if (action === 'ArrowDown') {
+                    player.current.setX(x + 1);
+                    player.current.draw(context);
+
+                    // xPos = xPos + 1;
+                } else if (action === 'ArrowLeft') {
+                    player.current.setY(y - 1);
+                    player.current.draw(context);
+
+                    // yPos = yPos - 1;
+                } else if (action === 'ArrowRight') {
+                    player.current.setY(y + 1);
+                    player.current.draw(context);
+
+                    // yPos = yPos + 1;
+                }
+                // gameLoop();
+            } else {
+                otherPlayers.forEach((newPlayer) => {
+                    if (newPlayer.getUserId() === id) {
+                        if (action === 'ArrowUp') {
+                            newPlayer.setX(x - 1);
+                            newPlayer.draw(context);
+                            newPlayer.setY(y);
+                            newPlayer.draw(context);
+                            newPlayer.setAction(action);
+                        } else if (action === 'ArrowDown') {
+                            newPlayer.setX(x + 1);
+                            newPlayer.draw(context);
+                            newPlayer.setY(y);
+                            newPlayer.draw(context);
+                            newPlayer.setAction(action);
+                            newPlayer.draw(context);
+                        } else if (action === 'ArrowLeft') {
+                            newPlayer.setY(y - 1);
+                            newPlayer.draw(context);
+                            newPlayer.setX(x);
+                            newPlayer.draw(context);
+                            newPlayer.setAction(action);
+                            newPlayer.draw(context);
+                        } else if (action === 'ArrowRight') {
+                            newPlayer.setY(y + 1);
+                            newPlayer.draw(context);
+                            newPlayer.setX(x);
+                            newPlayer.draw(context);
+                            newPlayer.setAction(action);
+                            newPlayer.draw(context);
+                        }
+                        // gameLoop();
+
+                    }
+                });
+            }
+        }
+
+        const startGameLoop = () => {
+            requestAnimationFrame(() => gameLoop());
+        }
+
+        const gameLoop = () => {
+            clearScreen();
+            drawGrid(map);
+            player.current.draw(context);
+            // player.draw(context);
+            // console.log('ZZZZZZZZZZZZZZZZZZZZZZZ');
+            if(otherPlayers.length > 0) {
+                otherPlayers.forEach((newPlayer) => {
+                    // console.log('GGGGGGGGAME LOOP Other Players ', newPlayer.getUserId() + ' X ', newPlayer.getX() + ' Y ', newPlayer.getY() + ' Color ', newPlayer.getColor());
+                    newPlayer.draw(context);
+                });
+            }
+            requestAnimationFrame(() => gameLoop());
+        }
+
+        const clearScreen = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        if (!context) {
+            console.error("Could not get canvas context");
+            return;
+
+        } else {
+            drawGrid(map);
+        }
+
+        // initializeWebSocket();
+        startGameLoop();
+
+        function initializeWebSocket() {
+            const socket = new SockJS("http://localhost:8080/gs-guide-websocket");
+            const client = Stomp.over(socket);
+            let id = getUserId();
+            client.connect({}, () => {
+                // client.subscribe(`/topic/movement/${id}` , (message) => {
+                client.subscribe('/topic/movement/', (message) => {
+
+                    const response = JSON.parse(message.body);
+                    // callback(response);
+                });
+            });
+        };
+
+        function drawGrid(grid: number[][]) {
+            let cellSize = 25;
+            const context = canvasRef.current.getContext('2d');
+
+            let x = player.current.getX();
+            let y = player.current.getY();
+
+            for (let row = 0; row < grid.length; row++) {
+                for (let col = 0; col < grid[row].length; col++) {
+                    const value = grid[row][col];
+
+                    for(let i = 0; i < otherPlayers.length; i++) {
+                        if (row === otherPlayers[i].getX() && col === otherPlayers[i].getY()) {
+                            context.fillStyle = otherPlayers[i].getColor();
+                        }
+                    }
+
+                    if (row === x && col === y) {
+                        context.fillStyle = player.current.getColor();
+                    } else if (grid[row][col] === 1) {
+                        context.fillStyle = 'black';
+                    } else if (grid[row][col] === 0) {
+                        context.fillStyle = 'white';
+                    } else if (grid[row][col] === 2) {
+                        context.fillStyle = 'red';
+                    }
+
+                    // for(let i = 0; i < otherPlayers.length; i++) {
+                    //     if (row === otherPlayers[i].getX() && col === otherPlayers[i].getY()) {
+                    //         context.fillStyle = otherPlayers[i].getColor();
+                    //     }
+                    // }
+
+                    context.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+                    context.strokeStyle = 'grey';
+                    context.lineWidth = 1;
+                    context.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
+                }
+            }
+            // window.addEventListener('keydown', (event) => {
+            //     handleMove(event.key);
+            // });
+            return () => {
+                // Bereinige hier, z.B. trenne die WebSocket-Verbindung
+                // window.removeEventListener('keydown', (event) => {
+                //     handleMove(event.key);
+                // });
+            };
+        }
+        window.addEventListener('keydown', (event) => {
+            handleMove(event.key);
+        });
+        }, []);
 
     return (
-        <div className={style.root} style={{ paddingTop, paddingLeft }}>
-            {subGrid.map((row, rowIndex) => (
-                <div key={rowIndex} className={style.row}>
-                    {row.map((cell, colIndex) => (
-                        <span key={colIndex} className={style.cell}>
-                            {rowIndex + startRow === xPos && colIndex + startCol === yPos ? '@' : cell}
-                        </span>
-                    ))}
-                </div>
-            ))}
+        <div>
+            <canvas ref={canvasRef} width="775" height="600" style={{ backgroundColor: 'blue' }}/>
         </div>
     );
+};
+
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export default MapGrid;
+export default GameComponent;
