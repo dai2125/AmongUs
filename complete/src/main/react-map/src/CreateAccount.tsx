8011f702aae5b2d5fc676c649ae2a7e91a59efa3
Anas Taha <ticoreck56@gmail.com>
2024-04-09 23:04:15 +0200

    more styling for log in and create account pages

--- complete/src/main/react-map/src/CreateAccount.tsx
+++ complete/src/main/react-map/src/CreateAccount.tsx
@@ -6,27 +6,31 @@ type Props = {
 export default function CreateAccount({onCreateClick, onLogInNavClick}, Props){
 
     return(
-        <div className="grid grid-rows-10 bg-transparent border-double rounded-lg border-2 border-fuchsia-800 w-1/2 h-96">
-            <div className="row-span-2 flex items-center justify-center"><b>Create Account</b></div>
-            <div className="row-span-7 justify-self-center">
-                <form className="p-3">
-                    <div>
-                        <label>E-mail:</label><br/>
-                        <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
-                        <label>PassWord:</label><br/>
-                        <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
-                        <label>Confirm PassWord:</label><br/>
-                        <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
-                    </div>
+        <div className="background">
+            <div className="flex items-center justify-center h-screen">
+                <div className="grid grid-rows-10 bg-transparent border-double rounded-lg border-2 border-fuchsia-800 w-1/2 h-96">
+                    <div className="row-span-2 flex items-center justify-center text-white"><b>Create Account</b></div>
+                    <div className="row-span-7 justify-self-center">
+                        <form className="p-3">
+                            <div>
+                                <label>E-mail:</label><br/>
+                                <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
+                                <label>PassWord:</label><br/>
+                                <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
+                                <label>Confirm PassWord:</label><br/>
+                                <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
+                            </div>
 
-                    <div className="flex justify-center">
-                        <button onClick={onCreateClick} className="bg-blue-500 hover:bg-gray-400 text-slate-50 font-bold py-2 px-4 rounded mt-3" type="submit">Log-In</button>
+                            <div className="flex justify-center">
+                                <button onClick={onCreateClick} className="bg-blue-500 hover:bg-gray-400 text-slate-50 font-bold py-2 px-4 rounded mt-3 text-white" type="submit">Sign-Up</button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-            <div className="row-span-1 flex justify-between px-4">
-                <p className="">Already have an account?</p>
-                <button onClick={onLogInNavClick} className="underline text-blue-500">Log-in</button>
+                    <div className="row-span-1 flex justify-between px-4">
+                        <p className="text-white">Already have an account?</p>
+                        <button onClick={onLogInNavClick} className="underline text-blue-500">Log-in</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- complete/src/main/react-map/src/Log-in.tsx
+++ complete/src/main/react-map/src/Log-in.tsx
@@ -9,25 +9,29 @@ export default function LogIn({onLogIn, onCreateAccountNav}: Props){
 
 
     return(
-        <div className="grid grid-rows-10 bg-transparent border-double rounded-lg border-2 border-fuchsia-800 w-1/2 h-96">
-            <div className="row-span-2 flex items-center justify-center"><b>LOG-IN</b></div>
-            <div className="row-span-7 justify-self-center">
-                <form className="p-3">
-                    <div>
-                        <label>E-mail:</label><br/>
-                        <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
-                        <label>PassWord:</label><br/>
-                        <input name="body" className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
-                    </div>
+        <div className="background">
+            <div className="flex items-center justify-center h-screen">
+                <div className="grid grid-rows-10 bg-opacity-75 border-double rounded-lg border-2 border-fuchsia-800 w-1/2 h-96">
+                    <div className="row-span-2 flex items-center justify-center text-white"><b>LOG-IN</b></div>
+                    <div className="row-span-7 justify-self-center">
+                        <form className="p-3">
+                            <div>
+                                <label>E-mail:</label><br/>
+                                <input className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
+                                <label>PassWord:</label><br/>
+                                <input name="body" className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" /><br/>
+                            </div>
 
-                    <div className="flex justify-center">
-                        <button onClick={onLogIn} className="bg-blue-500 hover:bg-gray-400 text-slate-50 font-bold py-2 px-4 rounded mt-3" type="submit">Log-In</button>
+                            <div className="flex justify-center">
+                                <button onClick={onLogIn} className="bg-blue-500 hover:bg-gray-400 text-slate-50 font-bold py-2 px-4 rounded mt-3" type="submit">Log-In</button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-            <div className="row-span-1 flex justify-between px-4">
-                <p className="">Don't have an Account?</p>
-                <button onClick={onCreateAccountNav} className="underline text-blue-500">Create Account</button>
+                    <div className="row-span-1 flex justify-between px-4">
+                        <p className="text-white">Don't have an Account?</p>
+                        <button onClick={onCreateAccountNav} className="underline text-blue-500">Create Account</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
