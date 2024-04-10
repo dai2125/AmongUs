ad7f4fc6052080fceba40a5efb4f67344f3f1f3d
DominikEnder <dai2125@students.fhv.at>
2024-04-10 09:59:07 +0200

    Logic adjustments

--- complete/src/main/java/com/example/messagingstompwebsocket/GamingController/MovementController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GamingController/MovementController.java
@@ -1,7 +1,7 @@
 package com.example.messagingstompwebsocket.GamingController;
 
-import com.example.messagingstompwebsocket.DataModel.MovementResponse;
 import com.example.messagingstompwebsocket.DataModel.User;
+import com.example.messagingstompwebsocket.DataModel.UserMovement;
 import com.example.messagingstompwebsocket.Map.DefaultMap;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,10 +13,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.*;
-
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,9 +56,9 @@ public class MovementController {
             user.setX(r.nextInt(5) + 2);
             user.setY(r.nextInt(5) + 2);
             System.out.println("REGISTER a new User: " + user.toString());
-            MovementResponse movementResponse = new MovementResponse(user.getAction(), user.getUserId(), user.getColor(), user.getX(), user.getY());
+            UserMovement userMovement = new UserMovement(user.getUserId(), user.getAction(), user.getColor(), user.getX(), user.getY());
 
-            messagingTemplate.convertAndSend("/topic/register/", new ObjectMapper().writeValueAsString(movementResponse));
+            messagingTemplate.convertAndSend("/topic/register/", new ObjectMapper().writeValueAsString(userMovement));
         }
         if(counter ==  colors.length - 1) {
             counter = 0;
@@ -74,23 +71,23 @@ public class MovementController {
 
         if (user.getAction().equals("ArrowUp") && !defaultMap.isWall(user.getY() - 1, user.getX())) {
             user.setY(user.getY() - 1);
-            MovementResponse movementResponse = new MovementResponse(user.getAction(), user.getUserId() , user.getColor(), user.getX(), user.getY());
-            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(movementResponse));
+            UserMovement userMovement = new UserMovement(user.getUserId(), user.getAction(), user.getColor(), user.getX(), user.getY());
+            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(userMovement));
             System.out.println("Movement: User=" + user.getUserId() + " moved=" + user.getX() + ", " + user.getY());
         } else if (user.getAction().equals("ArrowDown") && !defaultMap.isWall(user.getY() + 1, user.getX())) {
             user.setY(user.getY() + 1);
-            MovementResponse movementResponse = new MovementResponse(user.getAction(), user.getUserId() , user.getColor(), user.getX(), user.getY());
-            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(movementResponse));
+            UserMovement userMovement = new UserMovement(user.getUserId(), user.getAction(), user.getColor(), user.getX(), user.getY());
+            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(userMovement));
             System.out.println("Movement: User=" + user.getUserId() + " moved=" + user.getX() + ", " + user.getY());
         } else if (user.getAction().equals("ArrowLeft") && !defaultMap.isWall(user.getY(), user.getX() - 1)) {
             user.setX(user.getX() - 1);
-            MovementResponse movementResponse = new MovementResponse(user.getAction(), user.getUserId() , user.getColor(), user.getX(), user.getY());
-            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(movementResponse));
+            UserMovement userMovement = new UserMovement(user.getUserId(), user.getAction(), user.getColor(), user.getX(), user.getY());
+            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(userMovement));
             System.out.println("Movement: User=" + user.getUserId() + " moved=" + user.getX() + ", " + user.getY());
         } else if (user.getAction().equals("ArrowRight") && !defaultMap.isWall(user.getY(), user.getX() + 1)) {
             user.setX(user.getX() + 1);
-            MovementResponse movementResponse = new MovementResponse(user.getAction(), user.getUserId() , user.getColor(), user.getX(), user.getY());
-            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(movementResponse));
+            UserMovement userMovement = new UserMovement(user.getUserId(), user.getAction(), user.getColor(), user.getX(), user.getY());
+            messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(userMovement));
             System.out.println("Movement: User=" + user.getUserId() + " moved=" + user.getX() + ", " + user.getY());
         }
 
@@ -98,6 +95,5 @@ public class MovementController {
             logger.warn("New user connected: {}"/*, userId*/);
             currentUsers.add(user);
         }
-
     }
 }
