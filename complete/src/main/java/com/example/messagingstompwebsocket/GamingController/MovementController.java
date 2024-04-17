6a5d9003d1366483040b50489a92560eccfa19b6
DominikEnder <dai2125@students.fhv.at>
2024-04-17 06:30:26 +0200

    REST Controller login and sign up PersonService Validation JPA Repository Exception handling DTO login and sign up Person data model

--- complete/src/main/java/com/example/messagingstompwebsocket/GamingController/MovementController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GamingController/MovementController.java
@@ -2,12 +2,10 @@ package com.example.messagingstompwebsocket.GamingController;
 
 import com.example.messagingstompwebsocket.DataModel.User;
 import com.example.messagingstompwebsocket.DataModel.UserMovement;
-import com.example.messagingstompwebsocket.Map.DefaultMap;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -16,7 +14,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.*;
-import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,23 +24,26 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @PropertySource("classpath:application.properties")
 public class MovementController {
 
+    private SimpMessagingTemplate messagingTemplate;
+    private MovementService movementService = new MovementService();
+    private static final Logger logger = LoggerFactory.getLogger(MovementController.class);
+
+    String[] colors = {"#FF0000", "#FF8000", "#0080FF", "#FF00FF", "#FF007F", "#808080"};
+    List<User> userList = new ArrayList<>();
+    Random r = new Random();
+    private int counter = 0;
+
+
     public MovementController(SimpMessagingTemplate messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(MovementController.class);
+    private RegisterService registerService = new RegisterService(messagingTemplate);
 
-    private final HashSet<User> currentUsers = new HashSet<>();
-    private final SimpMessagingTemplate messagingTemplate;
-
-    private MovementService movementService = new MovementService();
-    private DefaultMap defaultMap = new DefaultMap();
-    ArrayList<User> userArrayList = new ArrayList<>();
 
     @EventListener
     public void sessionConnectEvent(SessionConnectEvent event) throws InterruptedException, JsonProcessingException {
         SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
-
         System.out.println("SessionConnectEvent headers.getSessiondId() : " + headers.getSessionId());
         String sessionId = headers.getSessionId();
      }
@@ -54,52 +54,63 @@ public class MovementController {
         System.out.println("SessionDisconnectEvent : " + event.toString());
         event.getSessionId();
 
-        for(User u : userArrayList) {
+        for(User u : userList) {
             if(u.getUserId().equals(event.getSessionId())) {
                 u.setAction("offline");
                 UserMovement userMovement = new UserMovement(u.getAction(), u.getUserId(), u.getColor(), u.getX(), u.getY());
-                messagingTemplate.convertAndSend("/topic/disconnected/", new ObjectMapper().writeValueAsString(userMovement));
-                userArrayList.remove(u);
+                messagingTemplate.convertAndSend("/topic/disconnected/",
+                                                    new ObjectMapper().writeValueAsString(userMovement));
+                userList.remove(u);
                 break;
             }
         }
         messagingTemplate.convertAndSend("/topic/disconnected/", event.getSessionId());
     }
 
-    String[] colors = {"#FF0000", "#FF8000", "#0080FF", "#FF00FF", "#FF007F", "#808080"};
-    Random r = new Random();
-    int counter = 0;
+
 
     @MessageMapping("/register/")
     @SendTo("/topic/register/")
     public void register(@Payload User user, SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws JsonProcessingException {
-        if(user.getUserId().isEmpty()) {
-            user.setUserId(simpMessageHeaderAccessor.getSessionId());
-            user.setColor(colors[counter++]);
-            user.setAction("null");
-            user.setX(r.nextInt(5) + 2);
-            user.setY(r.nextInt(5) + 2);
-
-            userArrayList.add(user);
-
-            UserMovement userMovement = new UserMovement(user.getAction(), user.getUserId(), user.getColor(), user.getX(), user.getY());
-
-            messagingTemplate.convertAndSend("/topic/register/", new ObjectMapper().writeValueAsString(userMovement));
-            }
-
-        for(int i = 0; i < userArrayList.size(); i++) {
-            UserMovement userMovement1 = new UserMovement(userArrayList.get(i).getAction(), userArrayList.get(i).getUserId(), userArrayList.get(i).getColor(), userArrayList.get(i).getX(), userArrayList.get(i).getY());
-            messagingTemplate.convertAndSend("/topic/connected/", new ObjectMapper().writeValueAsString(userMovement1));
-        }
-        if(counter ==  colors.length - 1) {
-            counter = 0;
-        }
+        registerService.registerUser(user, simpMessageHeaderAccessor);
+        registerService.updateAllUserWithTheNewUser();
+//        if(user.getUserId().isEmpty()) {
+//            user.setUserId(simpMessageHeaderAccessor.getSessionId());
+//            user.setColor(colors[counter++]);
+//            user.setAction("null");
+//            user.setX(r.nextInt(5) + 2);
+//            user.setY(r.nextInt(5) + 2);
+//
+//            userList.add(user);
+//
+//            UserMovement userMovement = new UserMovement(user.getAction(), user.getUserId(), user.getColor(), user.getX(), user.getY());
+//
+//            messagingTemplate.convertAndSend("/topic/register/",
+//                                                new ObjectMapper().writeValueAsString(userMovement));
+//            }
+//
+//        for(int i = 0; i < userList.size(); i++) {
+//            UserMovement userMovement1 = new UserMovement(userList.get(i).getAction(), userList.get(i).getUserId(), userList.get(i).getColor(), userList.get(i).getX(), userList.get(i).getY());
+//            messagingTemplate.convertAndSend("/topic/connected/",
+//                                                new ObjectMapper().writeValueAsString(userMovement1));
+//        }
+//        if(counter ==  colors.length - 1) {
+//            counter = 0;
+//        }
     }
 
     @MessageMapping("/movement/{userId}")
     @SendTo("/topic/movement/")
     public void processMovement(@Payload User user, SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws JsonProcessingException {
-        messagingTemplate.convertAndSend("/topic/movement/", new ObjectMapper().writeValueAsString(movementService.wallCollision(user)));
+        messagingTemplate.convertAndSend("/topic/movement/",
+                                            new ObjectMapper().writeValueAsString(movementService.wallCollision(user)));
+    }
+
+    @MessageMapping("/movement/CLOSED/{userId}")
+//    @SendTo("/topic/movement/{userId}")
+    public void movementUserId(@Payload User user) throws JsonProcessingException {
+        messagingTemplate.convertAndSend("/topic/movement/{userId}",
+                                            new ObjectMapper().writeValueAsString(movementService.wallCollision(user)));
     }
 
 }
