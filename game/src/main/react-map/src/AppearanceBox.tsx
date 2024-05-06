be4f882342a5ac6a9422d8dbde4a2e9518d9d3fb
dai2125 <dai2125@students.fhv.at>
2024-05-06 15:38:08 +0200

    AppearanceBox

--- game/src/main/react-map/src/AppearanceBox.tsx
+++ game/src/main/react-map/src/AppearanceBox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../src/CSS/rainbowButton.css';
 import blue from '../src/images/Characters/Blue.jpg';
 import red from '../src/images/Characters/Red.jpg';
 import green from '../src/images/Characters/Green.jpg';
@@ -15,361 +16,262 @@ import gray from '../src/images/Characters/Gray.jpg';
 
 function AppearanceBox() {
 
-    const [showBlue, setShowBlue] = React.useState<boolean>(false);
     const [showRed, setShowRed] = React.useState<boolean>(false);
+    const [showBlue, setShowBlue] = React.useState<boolean>(false);
+    const [showCyan, setShowCyan] = React.useState<boolean>(false);
+    const [showGray, setShowGray] = React.useState<boolean>(false);
+    const [showPink, setShowPink] = React.useState<boolean>(false);
+    const [showLime, setShowLime] = React.useState<boolean>(false);
     const [showGreen, setShowGreen] = React.useState<boolean>(false);
+    const [showBlack, setShowBlack] = React.useState<boolean>(false);
+    const [showWhite, setShowWhite] = React.useState<boolean>(false);
+    const [showBrown, setShowBrown] = React.useState<boolean>(false);
     const [showOrange, setShowOrange] = React.useState<boolean>(false);
     const [showYellow, setShowYellow] = React.useState<boolean>(false);
     const [showPurple, setShowPurple] = React.useState<boolean>(false);
-    const [showBrown, setShowBrown] = React.useState<boolean>(false);
-    const [showCyan, setShowCyan] = React.useState<boolean>(false);
-    const [showLime, setShowLime] = React.useState<boolean>(false);
-    const [showPink, setShowPink] = React.useState<boolean>(false);
-    const [showBlack, setShowBlack] = React.useState<boolean>(false);
-    const [showWhite, setShowWhite] = React.useState<boolean>(false);
-    const [showGray, setShowGray] = React.useState<boolean>(false);
 
 
     const redButtonClicked = () => {
         setShowRed(true);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowBrown(false);
+        setShowGreen(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const orangeButtonClicked = () => {
         setShowOrange(true);
         setShowRed(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowGreen(false);
+        setShowBrown(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowPurple(false);
+        setShowYellow(false);
     }
 
     const yellowButtonClicked = () => {
         setShowYellow(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowGreen(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowBrown(false);
+        setShowOrange(false);
+        setShowPurple(false);
     }
 
     const greenButtonClicked = () => {
         setShowGreen(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
         setShowBlack(false);
+        setShowBrown(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const cyanButtonClicked = () => {
         setShowCyan(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
         setShowBlack(false);
+        setShowGreen(false);
+        setShowBrown(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const blueButtonClicked = () => {
         setShowBlue(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowBrown(false);
+        setShowGreen(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const purpleButtonClicked = () => {
         setShowPurple(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
         setShowBlack(false);
+        setShowGreen(false);
+        setShowBrown(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowOrange(false);
     }
 
     const brownButtonClicked = () => {
         setShowBrown(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowGreen(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const grayButtonClicked = () => {
         setShowGray(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowLime(false);
         setShowPink(false);
         setShowBlack(false);
+        setShowGreen(false);
         setShowWhite(false);
+        setShowBrown(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const limeButtonClicked = () => {
         setShowLime(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowPink(false);
+        setShowGreen(false);
+        setShowBrown(false);
         setShowBlack(false);
         setShowWhite(false);
+        setShowOrange(false);
+        setShowYellow(false);
+        setShowPurple(false);
     }
 
     const pinkButtonClicked = () => {
         setShowPink(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
+        setShowGreen(false);
         setShowBlack(false);
+        setShowBrown(false);
         setShowWhite(false);
+        setShowYellow(false);
+        setShowPurple(false);
+        setShowOrange(false);
     }
 
     const blackButtonClicked = () => {
         setShowBlack(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowGreen(false);
+        setShowBrown(false);
         setShowWhite(false);
+        setShowOrange(false);
+        setShowYellow(false);
+        setShowPurple(false);
     }
 
     const whiteButtonClicked = () => {
         setShowWhite(true);
         setShowRed(false);
-        setShowOrange(false);
-        setShowYellow(false);
-        setShowGreen(false);
         setShowCyan(false);
         setShowBlue(false);
-        setShowPurple(false);
-        setShowBrown(false);
         setShowGray(false);
         setShowLime(false);
         setShowPink(false);
+        setShowGreen(false);
         setShowBlack(false);
+        setShowBrown(false);
+        setShowOrange(false);
+        setShowYellow(false);
+        setShowPurple(false);
     }
-
-
 
     return (
         <div>
-            <h2>Appearance</h2>
-            <div>
-                {showBlue   ? <img src={blue}   /> : <div></div>}
-                {showRed    ? <img src={red}    /> : <div></div>}
+            <div className="character-image">
+                {showRed ? <img src={red} /> : <div></div>}
+                {showBlue ? <img src={blue} /> : <div></div>}
+                {showCyan ? <img src={cyan} /> : <div></div>}
+                {showGray ? <img src={gray} /> : <div></div>}
+                {showLime ? <img src={lime} /> : <div></div>}
+                {showPink ? <img src={pink} /> : <div></div>}
+                {showGreen ? <img src={green} /> : <div></div>}
+                {showBlack ? <img src={black} /> : <div></div>}
+                {showWhite ? <img src={white} /> : <div></div>}
+                {showBrown ? <img src={brown} /> : <div></div>}
                 {showOrange ? <img src={orange} /> : <div></div>}
                 {showYellow ? <img src={yellow} /> : <div></div>}
-                {showGreen  ? <img src={green}  /> : <div></div>}
-                {showCyan   ? <img src={cyan}   /> : <div></div>}
                 {showPurple ? <img src={purple} /> : <div></div>}
-                {showBrown  ? <img src={brown}  /> : <div></div>}
-                {showGray   ? <img src={gray}   /> : <div></div>}
-                {showLime   ? <img src={lime}   /> : <div></div>}
-                {showPink   ? <img src={pink}   /> : <div></div>}
-                {showBlack  ? <img src={black}  /> : <div></div>}
-                {showWhite  ? <img src={white}  /> : <div></div>}
 
             </div>
-            <button onClick={redButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'red',
-                color: 'white',
-                borderTopLeftRadius: '5px',
-                borderBottomLeftRadius: '5px',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={orangeButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'orange',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={yellowButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'yellow',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={greenButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'green',
-                color: 'white',
-
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={cyanButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'cyan',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={blueButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'blue',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={purpleButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'purple',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={brownButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'brown',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={grayButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'gray',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={limeButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'lime',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={pinkButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'pink',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={blackButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'black',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-            <button onClick={whiteButtonClicked} style={{
-                width: '25px',
-                height: '50px',
-                backgroundColor: 'white',
-                color: 'white',
-                borderTopRightRadius: '5px',
-                borderBottomRightRadius: '5px',
-                border: 'none',
-                cursor: 'pointer'
-            }}></button>
-
-
+            <div className="button-box">
+                <button className="red-button"      onClick={redButtonClicked}      ></button>
+                <button className="orange-button"   onClick={orangeButtonClicked}   ></button>
+                <button className="yellow-button"   onClick={yellowButtonClicked}   ></button>
+                <button className="green-button"    onClick={greenButtonClicked}    ></button>
+                <button className="cyan-button"     onClick={cyanButtonClicked}     ></button>
+                <button className="blue-button"     onClick={blueButtonClicked}     ></button>
+                <button className="purple-button"   onClick={purpleButtonClicked}   ></button>
+                <button className="brown-button"    onClick={brownButtonClicked}    ></button>
+                <button className="gray-button"     onClick={grayButtonClicked}     ></button>
+                <button className="lime-button"     onClick={limeButtonClicked}     ></button>
+                <button className="pink-button"     onClick={pinkButtonClicked}     ></button>
+                <button className="black-button"    onClick={blackButtonClicked}    ></button>
+                <button className="white-button"    onClick={whiteButtonClicked}    ></button>
+            </div>
         </div>
     );
 }
