e1698435545ab568f940947face0fa58b2f59afd
matty096 <mah5757@students.fhv.at>
2024-06-27 09:36:35 +0200

    create account appearance

--- game/src/main/react-map/src/MainPage/CreateAccount.tsx
+++ game/src/main/react-map/src/MainPage/CreateAccount.tsx
@@ -96,20 +96,20 @@ export default function CreateAccount({onLoginNavClick}: Props){
                             <div>
                                 <label className="text-white">Username:</label><br/>
                                 <input name="name"
-                                       className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
+                                       className="input-field-name bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
                                        style={{width: "505"}}
                                        required/><br/>
                                 <label className="text-white">E-mail:</label><br/>
                                 <input name="email"
-                                       className="input-field w-72 h-10 bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
+                                       className="input-field-name bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
                                        required/><br/>
                                 <label className="text-white">Password:</label><br/>
                                 <input type="password" name="password"
-                                       className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
+                                       className="input-field-name bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
                                        required/><br/>
                                 <label className="text-white">Confirm Password:</label><br/>
                                 <input type="password" name="passwordConfirm"
-                                       className="input-field bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
+                                       className="input-field-name bg-white border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 bg-opacity-20 text-white"
                                        required/><br/>
                             </div>
 
