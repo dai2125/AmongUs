3ef52ef722b6baa07a645bcfa92812c15a5d5ee5
Anas Taha <ticoreck56@gmail.com>
2024-07-20 19:44:13 +0200

    Tasklist restyle

--- game/src/main/react-map/src/GameComponents/TaskList.tsx
+++ game/src/main/react-map/src/GameComponents/TaskList.tsx
@@ -16,19 +16,31 @@ const TaskList = ({role, tasks}) => {
     }, [tasks]);
 
     return (
-        <div className="absolute top-36 left-1 w-100 h-100 bg-white bg-opacity-80 border  px-4 py-1"
-             style={{backgroundColor: 'rgba(255, 255, 255, 0.8)', display: 'flex', alignItems: 'center', zIndex: 1}}>
-            <div style={{flex: 1, minHeight: '100%', borderRight: '1px solid #ccc', fontFamily: "'VCR OSD Mono', monospace"}}>
+        <div className="absolute bottom-1.5 left-1 w-100 h-100 bg-white bg-opacity-80 border  px-4 py-1"
+             style={{
+                 backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                 display: 'flex',
+                 alignItems: 'center',
+                 zIndex: 1,
+                 borderRadius: "5px"
+             }}>
+            <div style={{flex: 1, minHeight: '100%', fontFamily: "'VCR OSD Mono', monospace"}}>
                 <p className="text-black bold text-xl">{myRole}</p>
                 {myTasks.map((task, index) => (
                     <p key={index}>{task}</p>
                 ))}
             </div>
-            <div style={{ marginLeft: 20, whiteSpace: 'nowrap', writingMode: 'vertical-lr', transform: 'rotate(180deg)', fontFamily: "'VCR OSD Mono', monospace"}}>
-                 <h1>Tasks</h1><br/>
-             </div>
-         </div>
-     );
+            <div style={{
+                marginLeft: 20,
+                whiteSpace: 'nowrap',
+                writingMode: 'vertical-lr',
+                transform: 'rotate(180deg)',
+                fontFamily: "'VCR OSD Mono', monospace"
+            }}>
+                <h1>Tasks</h1><br/>
+            </div>
+        </div>
+    );
 }
 
 export default TaskList;
